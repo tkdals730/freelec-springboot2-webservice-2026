9436de7cc679a8f0466f46f468027f3c87bbe2b3
Sangmin <tkdals730@naver.com>
2026-04-27 15:31:25 +0900

    롬복 검증

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,51 @@
+package com.jojoldu.book.springboot.web.dto;
+
+
+import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.is;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class HelloResponseDtoTest {
+
+    @Test
+
+    public void 롬복_기능_테스트() {
+
+        //given
+
+        String name = "test";
+
+        int amount = 1000;
+
+        //when
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+
+        // assertj라는 테스트 검증 라이브러리의 검증 메소드입니다.
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(
+                get("/hello.dto"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)))
+                .andExpect(content().string(hello));
+    }
+}
