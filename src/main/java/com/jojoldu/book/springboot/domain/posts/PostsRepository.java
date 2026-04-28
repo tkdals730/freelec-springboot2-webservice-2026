7a67474771ff42a11cf43f3c5f8fe9bd0dca13f4
Sangmin <tkdals730@naver.com>
2026-04-28 11:38:32 +0900

    day02 feat : Postrepository 저장/조회 테스트 통과

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,4 +1,7 @@
 package com.jojoldu.book.springboot.domain.posts;
 
-public interface PostsRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
 }
--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,4 +1,44 @@
 package com.jojoldu.book.springboot.domain.posts;
 
-public class PostRepositoryTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@gmail.com")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+
 }
